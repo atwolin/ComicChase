1079f970473acfa7347908a48fda27128e377cd0
atwolin@linux <atwolin@gmail.com>
2025-11-12 21:42:09 +0800

    feat: define item models

--- comic_scrapers/comic_scrapers/items.py
+++ comic_scrapers/comic_scrapers/items.py
@@ -6,7 +6,37 @@
 import scrapy
 
 
-class ComicScrapersItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class TwComicItem(scrapy.Item):
+    # Anchor point field
+    isbn_tw = scrapy.Field()
+    title_jp = scrapy.Field()
+
+    title_tw = scrapy.Field()
+    author_tw = scrapy.Field()
+
+
+class JpComicItem(scrapy.Item):
+    # Anchor point field
+    title_jp = scrapy.Field()
+
+    author_jp = scrapy.Field()
+    status_jp = scrapy.Field()
+
+
+class OrphanVolumeItem(scrapy.Item):
+    # Anchor point field
+    isbn_tw = scrapy.Field()
+
+    volume_number = scrapy.Field()
+    release_date_tw = scrapy.Field()
+    publisher_tw = scrapy.Field()
+
+
+class JpVolumeItem(scrapy.Item):
+    # Anchor point field
+    title_jp = scrapy.Field()
+
+    volume_number = scrapy.Field()
+    release_date_jp = scrapy.Field()
+    isbn_jp = scrapy.Field()
+    publisher_jp = scrapy.Field()
