6c1824890332e6dda156ccf4863fc1559c01d630
atwolin@linux <atwolin@gmail.com>
2025-11-13 20:37:10 +0800

    feat: add parse_volume_info() method to extract detailed volume information and store into OrphanVolumeItem()

--- comic_scrapers/comic_scrapers/spiders/books_spider.py
+++ comic_scrapers/comic_scrapers/spiders/books_spider.py
@@ -21,3 +21,32 @@ class BooksSpider(scrapy.Spider):
         for link in links:
             yield Request(url=link, callback=self.parse_volume_info)
             break  # For testing, remove this line to scrape all links
+
+    def parse_volume_info(self, response: Response):
+        """
+        Extract volume information from the book link
+        Information includes volume_number, release_date_tw, isbn_tw, publisher_tw
+        """
+        item = OrphanVolumeItem()
+        item['source_url'] = response.url
+
+        try:
+            isbn_tw = response.xpath("//div[@class='bd']/ul/li[contains(text(), 'ISBN：')]/text()").get()
+
+            volume_number = response.xpath("//div[@class='mod type02_p002 clearfix']/h1/text()").get()
+
+            release_date_tw = response.xpath("//div[@class='type02_p003 clearfix']/ul/li[contains(text(), '出版日期：')]/text()").get()
+
+            publisher_tw = response.xpath("//div[@class='type02_p003 clearfix']/ul/li[contains(text(), '出版社：')]")
+            publisher_tw = publisher_tw.xpath("./a[1]/span/text()").get() if publisher_tw else None
+
+            item['isbn_tw'] = isbn_tw
+            item['volume_number'] = volume_number
+            item['release_date_tw'] = release_date_tw
+            item['publisher_tw'] = publisher_tw
+
+        except AttributeError as e:
+            self.logger.error(f"Error parsing volume info from {response.url}: {e}")
+
+        finally:
+            yield item
