3fc76dee40c786283756ba709a1d81c52b83c0ac
atwolin@linux <atwolin@gmail.com>
2025-11-13 20:35:36 +0800

    fix: correct spider initialization and adding logging info instead of printing in console in BooksSpider()

--- comic_scrapers/comic_scrapers/spiders/books_spider.py
+++ comic_scrapers/comic_scrapers/spiders/books_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import re
 from scrapy.http import Request, Response
 
 from comic_scrapers.items import OrphanVolumeItem
@@ -6,16 +7,17 @@ from comic_scrapers.items import OrphanVolumeItem
 class BooksSpider(scrapy.Spider):
     name = "books_com"
 
-    urls = [
+    start_urls = [
         "https://www.books.com.tw/web/sys_compub/books/16/?loc=P_0001_017",
     ]
 
-    def parse(self, response: Request):
+    def parse(self, response: Response):
         """
         Obtain 100 book links from the books.com.tw new releases page
         """
         links = response.xpath("//div[@class='type02_bd-a']/h4/a/@href").getall()
-        print(f"Found {len(links)} book links on the page.")
-        yield from response.follow_all(links, self.parse_volume_info)
-
-
+        self.logger.info(f"Found {len(links)} book links on the page.")
+        # yield from response.follow_all(links, self.parse_volume_info)
+        for link in links:
+            yield Request(url=link, callback=self.parse_volume_info)
+            break  # For testing, remove this line to scrape all links
