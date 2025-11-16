965b3701183726c014d080284b9068ac8d3984ad
atwolin@linux <atwolin@gmail.com>
2025-11-16 16:04:44 +0800

    style: update Comic model to replace publisher_japan_original with publisher_jp and adjust admin configurations

--- src/comic/admin.py
+++ src/comic/admin.py
@@ -11,17 +11,17 @@ class PublisherAdmin(admin.ModelAdmin):
 @admin.register(Comic)
 class ComicAdmin(admin.ModelAdmin):
     list_display = (
-        'title_tw', 
-        'title_jp', 
-        'author_jp', 
+        'title_tw',
+        'title_jp',
+        'author_jp',
         'author_tw',
         'status_jp',
-        'publisher_japan_original'
+        'publisher_jp'
     )
-    list_filter = ('status_jp', 'publisher_japan_original')
+    list_filter = ('status_jp', 'publisher_jp')
     search_fields = (
-        'title_jp', 
-        'title_tw', 
+        'title_jp',
+        'title_tw',
         'author_jp',
         'author_tw'
     )
@@ -29,18 +29,17 @@ class ComicAdmin(admin.ModelAdmin):
 
 @admin.register(Volume)
 class VolumeAdmin(admin.ModelAdmin):
-    
+
     list_display = (
-        'comic', 
-        'volume_number', 
+        'comic',
+        'volume_number',
         'release_date_jp',
         'release_date_tw',
-        'publisher_tw'     
+        'publisher_tw'
     )
     list_filter = ('publisher_jp', 'publisher_tw')
     autocomplete_fields = ['comic', 'publisher_jp', 'publisher_tw'] # 修正
     search_fields = (
         'comic__title_jp',
-        'comic__title_tw'  
+        'comic__title_tw'
     )
-    
