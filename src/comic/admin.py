894aece252aac0f3a6b493deab9f36e63a77c976
atwolin@linux <atwolin@gmail.com>
2025-11-20 21:32:06 +0800

    feat: add VolumeJp and VolumeTw models with admin registration

--- src/comic/admin.py
+++ src/comic/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Publisher, Comic, Volume
+from .models import Publisher, Comic, Volume, VolumeJp, VolumeTw
 
 @admin.register(Publisher)
 class PublisherAdmin(admin.ModelAdmin):
@@ -41,4 +41,36 @@ class VolumeAdmin(admin.ModelAdmin):
     search_fields = (
         'comic__title_jp',
         'comic__title_tw'
+    )
+
+@admin.register(VolumeJp)
+class VolumeJpAdmin(admin.ModelAdmin):
+    list_display = (
+        'title_jp',
+        'volume_number',
+        'isbn_jp',
+        'release_date_jp',
+        'publisher_jp',
+        'comic'
+    )
+    list_filter = ('publisher_jp',)
+    autocomplete_fields = ['comic', 'publisher_jp']
+    search_fields = (
+        'comic__title_jp',
+    )
+
+@admin.register(VolumeTw)
+class VolumeTwAdmin(admin.ModelAdmin):
+    list_display = (
+        'title_tw',
+        'volume_number',
+        'isbn_tw',
+        'release_date_tw',
+        'publisher_tw',
+        'comic'
+    )
+    list_filter = ('publisher_tw',)
+    autocomplete_fields = ['comic', 'publisher_tw']
+    search_fields = (
+        'comic__title_tw',
     )
