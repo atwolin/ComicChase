66a46980a1c51b9cbc5cd69876a311afb36106c0
atwolin@linux <atwolin@gmail.com>
2025-12-05 22:30:32 +0800

    fix: comment out STATICFILES_DIRS as the directory doesn't exist

--- app/src/config/settings/base.py
+++ app/src/config/settings/base.py
@@ -103,7 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+# STATICFILES_DIRS = [BASE_DIR / "static"]  # the directory doesn't exist
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
