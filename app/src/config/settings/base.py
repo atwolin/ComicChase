6e608004178b400b474b56016d4b76394f3aeff7
atwolin@linux <atwolin@gmail.com>
2025-12-06 23:14:40 +0800

    fix: update Nginx configuration for backend server, including /api/ and /admin/

--- app/src/config/settings/base.py
+++ app/src/config/settings/base.py
@@ -102,7 +102,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/django-static/"
 # STATICFILES_DIRS = [BASE_DIR / "static"]  # the directory doesn't exist
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
