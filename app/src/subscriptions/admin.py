2a43f72b358c18e86e15d69c68e566a939e6cf77
atwolin@linux <atwolin@gmail.com>
2025-12-23 22:33:14 +0800

    feat: register Subscription model with custom admin interface

--- app/src/subscriptions/admin.py
+++ app/src/subscriptions/admin.py
@@ -1,1 +1,10 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Subscription
+
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ("user", "series", "receive_email", "receive_line")
+
+
+admin.site.register(Subscription, SubscriptionAdmin)
