2511f6fabe03dd93712e52b4a8c11f0c6fc5f057
atwolin@linux <atwolin@gmail.com>
2025-12-24 21:02:56 +0800

    fix: remove auto-generation of username from email in CustomUserCreationForm to reduce complexity and potential errors

--- app/src/accounts/forms.py
+++ app/src/accounts/forms.py
@@ -7,21 +7,6 @@ class CustomUserCreationForm(UserCreationForm):
         model = get_user_model()
         fields = ("email",)
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        # Auto-generate username from email if not provided
-        if not user.username:
-            user.username = user.email.split("@")[0]
-            # Ensure uniqueness by appending a number if needed
-            base_username = user.username
-            counter = 1
-            while get_user_model().objects.filter(username=user.username).exists():
-                user.username = f"{base_username}{counter}"
-                counter += 1
-        if commit:
-            user.save()
-        return user
-
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
