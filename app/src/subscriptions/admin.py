3f87baac42bedb88229e92599ca1e531cd2b669d
atwolin@linux <atwolin@gmail.com>
2025-12-24 20:47:10 +0800

    fix: update SubscriptionAdmin list_display to use list syntax

--- app/src/subscriptions/admin.py
+++ app/src/subscriptions/admin.py
@@ -4,7 +4,9 @@ from .models import Subscription
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ("user", "series", "receive_email", "receive_line")
+    list_display = ["user", "series", "receive_email", "receive_line"]
+    list_filter = ["created_at"]
+    search_fields = ["user__username", "series__title"]
 
 
 admin.site.register(Subscription, SubscriptionAdmin)
