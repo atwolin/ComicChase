208d797c8cd07a4decd82755c3e5ec13e46f124d
atwolin@linux <atwolin@gmail.com>
2026-01-20 14:08:25 +0800

    fix: add SessionAuthentication to REST framework settings for identifying user

--- app/src/config/settings/base.py
+++ app/src/config/settings/base.py
@@ -251,6 +251,9 @@ CELERY_TASK_ROUTES = (
 # ============================================================
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+    ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
