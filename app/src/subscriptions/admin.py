67b1dd991b715206b5e2b14fa178af406e5de113
amelia <amelia322468@gmail.com>
2026-02-18 06:38:03 +0800

    feat(subscriptions): register NotificationLog in admin

--- app/src/subscriptions/admin.py
+++ app/src/subscriptions/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Subscription
+from .models import NotificationLog, Subscription
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
@@ -9,4 +9,12 @@ class SubscriptionAdmin(admin.ModelAdmin):
     search_fields = ["user__username", "series__title"]
 
 
+class NotificationLogAdmin(admin.ModelAdmin):
+    list_display = ["user", "volume", "sent_at"]
+    list_filter = ["sent_at"]
+    search_fields = ["user__username", "volume__series__title_tw"]
+    readonly_fields = ["user", "volume", "sent_at"]
+
+
 admin.site.register(Subscription, SubscriptionAdmin)
+admin.site.register(NotificationLog, NotificationLogAdmin)
